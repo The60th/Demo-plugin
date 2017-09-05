550105340c088185807a45294b1f73033bd8a842
JustinFTC8397 <justinthe60th@gmail.com>
2017-09-05 18:47:29 -0400

    First commit

---
+++ src/com/worldciv/the60th/Main.java
@@ -0,0 +1,10 @@
+package com.worldciv.the60th;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
