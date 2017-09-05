c4f612685658a02052d7a2fd5f1dc44339c48119
JustinFTC8397 <justinthe60th@gmail.com>
2017-09-05 19:29:43 -0400

    My demo change

--- src/com/worldciv/the60th/Main.java
+++ src/com/worldciv/the60th/Main.java
@@ -1,10 +1,17 @@
 package com.worldciv.the60th;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
 public class Main extends JavaPlugin{
+    FileConfiguration config = getConfig();
+    private static Plugin plugin;
 
     public void onEnable() {
 
@@ -14,19 +21,28 @@ public class Main extends JavaPlugin{
         logger.info(pdfFile.getName()
                 + "has successfully enabled. The current version is: "
                 + pdfFile.getVersion());
-
-        // saves a config
-        getConfig().options().copyDefaults(true);
-        saveConfig();
-
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(this, this);
     }
 
     public void onDisable() {
         PluginDescriptionFile pdfFile = getDescription();
         Logger logger = Logger.getLogger("Minecraft");
-
         logger.info(pdfFile.getName() + "has successfully disabled.");
+    }
+
+    public void registerCommands(){
+        //this.getCommand("name").setExecutor(new class());
+    }
+    public void registerEvents(){
+        //PluginManager pm = getServer().getPluginManager();
+        //pm.registerEvents(class , this);
+    }
+    private void registerPermissons(){
+        //PluginManager pm = getServer().getPluginManager();
+        // Permission p = new Permission("Permisson name");
+        //pm.addPermission(p);
+
+    }
+    public static Plugin getPlugin() {
+        return plugin;
     }
 }
