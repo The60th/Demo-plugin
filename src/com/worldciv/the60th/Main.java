4cb444545c235a1f61f28a1c7e3a793211839066
LegendaryPrime <LegendaryPrime@users.noreply.github.com>
2017-09-05 16:21:10 -0700

    onEnable and onDisable

--- src/com/worldciv/the60th/Main.java
+++ src/com/worldciv/the60th/Main.java
@@ -2,11 +2,31 @@ package com.worldciv.the60th;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class Main extends JavaPlugin{
 
-    public static void main(String[] args) {
-	// write your code here
-        // test
-        // My changes
+    public void onEnable() {
+
+        PluginDescriptionFile pdfFile = this.getDescription();
+        Logger logger = Logger.getLogger("Minecraft");
+
+        logger.info(pdfFile.getName()
+                + "has successfully enabled. The current version is: "
+                + pdfFile.getVersion());
+
+        // saves a config
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
+    }
+
+    public void onDisable() {
+        PluginDescriptionFile pdfFile = getDescription();
+        Logger logger = Logger.getLogger("Minecraft");
+
+        logger.info(pdfFile.getName() + "has successfully disabled.");
     }
 }
