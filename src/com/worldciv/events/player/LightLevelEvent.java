1dcf0c20546811fbf38f39a6329b2c6cfc4175bb
LegendaryPrime <LegendaryPrime@users.noreply.github.com>
2017-09-05 17:15:20 -0700

    Plugin YML basic elements for it functiion

--- src/com/worldciv/events/player/LightLevelEvent.java
+++ src/com/worldciv/events/player/LightLevelEvent.java
@@ -6,22 +6,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class LightLevelEvent implements Listener{
 
     public Player[] currentlyBlinded;
 
     @EventHandler
-    public void onMove(PlayerMoveEvent e) {
+    public void onMove(PlayerMoveEvent event) {
 
-        Player p = e.getPlayer();
-        Location l = p.getLocation();
-        Location vision =  new Location(l.getWorld(), l.getX(), l.getY() + 1, l.getZ());
+        Player player = event.getPlayer();
+        Location location = player.getLocation();
+        Location vision =  new Location(location.getWorld(), location.getX(), location.getY() + 1, location.getZ());
         int LightLevel = vision.getBlock().getLightLevel();
 
 
         if(LightLevel <= 4){
-            p.sendMessage(ChatColor.GOLD + "Your vision becomes unclear.");
+
+            player.sendMessage(ChatColor.GOLD + "Your vision becomes unclear.");
+            player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 9999, 10));
+            currentlyBlinded.add(p);
+
 
         }
 
