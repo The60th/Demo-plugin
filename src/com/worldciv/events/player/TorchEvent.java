4d4dfe3a1ac91f296931809ae53ad2f8c1095558
JustinFTC8397 <justinthe60th@gmail.com>
2017-09-05 21:53:28 -0400

    Offhand methods will be dealt with later.

--- src/com/worldciv/events/player/TorchEvent.java
+++ src/com/worldciv/events/player/TorchEvent.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.Torch;
 import org.bukkit.potion.PotionEffect;
@@ -22,7 +23,7 @@ public class TorchEvent implements Listener {
     public void onPlayerItemHeldEvent(PlayerItemHeldEvent event) {
         Player player = event.getPlayer();
         ItemStack itemStack = player.getInventory().getItem(event.getNewSlot());
-        if (itemStack == null || itemStack.getType() != Material.TORCH) {
+        if ((itemStack == null || itemStack.getType() != Material.TORCH) && player.getInventory().getItemInOffHand().getType() != Material.TORCH) {
             if (currentlyLit.contains(player)) {
                 currentlyLit.remove(player);
             }
@@ -46,4 +47,36 @@ public class TorchEvent implements Listener {
             }
         }
     }
+
+//    @EventHandler
+//    public void onPlayerSwapHandItemEvent(PlayerSwapHandItemsEvent event){
+//        Player player = event.getPlayer();
+//        ItemStack itemStackMain = event.getMainHandItem();
+//        ItemStack itemStackOff = event.getOffHandItem();
+//
+//        if ((itemStackMain == null && itemStackOff == null) || (itemStackMain.getType() != Material.TORCH && itemStackOff.getType() != Material.TORCH  )) {
+//            if (currentlyLit.contains(player)) {
+//                currentlyLit.remove(player);
+//                Bukkit.broadcastMessage("Offhand removing lit");
+//            }
+//            return;
+//        }
+//        Bukkit.broadcastMessage(player.getDisplayName() + " trigger torch event. Item in hand is " + itemStackMain);
+//        if (itemStackMain.getType() == Material.TORCH || itemStackOff.getType() == Material.TORCH  ) {
+//            Bukkit.broadcastMessage(player.getDisplayName() + " is holding a torch");
+//            if (LightLevelEvent.currentlyBlinded.contains(player)) {
+//                Bukkit.broadcastMessage(player.getDisplayName() + " is in currently blinded -- removing");
+//                LightLevelEvent.currentlyBlinded.remove(player);
+//                if (player.hasPotionEffect(PotionEffectType.BLINDNESS)) {
+//                    player.sendMessage(ChatColor.GOLD + "Your vision begins to clear up.");
+//                    currentlyLit.add(player);
+//                    Bukkit.broadcastMessage(player.getDisplayName() + " player is currently blindly -- removing");
+//                    player.removePotionEffect(PotionEffectType.BLINDNESS);
+//                    player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 5, 1));
+//                    player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 5, 1));
+//
+//                }
+//            }
+//        }
+//    }
 }
