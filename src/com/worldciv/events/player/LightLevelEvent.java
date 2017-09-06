622642195dcd0a7a2f97e6eac7330d68d6ca58e1
LegendaryPrime <LegendaryPrime@users.noreply.github.com>
2017-09-05 17:06:59 -0700

    Plugin YML basic elements for it functiion

--- src/com/worldciv/events/player/LightLevelEvent.java
+++ src/com/worldciv/events/player/LightLevelEvent.java
@@ -1,5 +1,29 @@
 package com.worldciv.events.player;
 
-public class LightLevelEvent {
-    //Demo
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class LightLevelEvent implements Listener{
+
+    public Player[] currentlyBlinded;
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+
+        Player p = e.getPlayer();
+        Location l = p.getLocation();
+        Location vision =  new Location(l.getWorld(), l.getX(), l.getY() + 1, l.getZ());
+        int LightLevel = vision.getBlock().getLightLevel();
+
+
+        if(LightLevel <= 4){
+            p.sendMessage(ChatColor.GOLD + "Your vision becomes unclear.");
+
+        }
+
+    }
 }
