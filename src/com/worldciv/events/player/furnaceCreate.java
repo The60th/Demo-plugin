0b037f12564233201e2c13d23f9f81ae270109ce
LegendaryPrime <LegendaryPrime@users.noreply.github.com>
2017-10-16 18:01:29 -0700

    furnace updates.

--- src/com/worldciv/events/player/furnaceCreate.java
+++ src/com/worldciv/events/player/furnaceCreate.java
@@ -2,13 +2,15 @@ package com.worldciv.events.player;
 
 import net.minecraft.server.v1_11_R1.EnumParticle;
 import net.minecraft.server.v1_11_R1.PacketPlayOutWorldParticles;
-import org.bukkit.*;
-import org.bukkit.block.Block;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Furnace;
 import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.FurnaceBurnEvent;
@@ -20,7 +22,6 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
-import static com.worldciv.the60th.MainTorch.plugin;
 import static com.worldciv.utility.utilityStrings.worldciv;
 
 public class furnaceCreate implements Listener {
@@ -33,173 +34,139 @@ public class furnaceCreate implements Listener {
 
         ItemStack[] itemsInFurnace = furnaceInv.getContents();
 
-        Material source = Material.EGG;
+        Material source = Material.IRON_INGOT;
         Material fuel = Material.COAL;
 
-        if (itemsInFurnace[0].getType() == source) {
-
-            if (itemsInFurnace[0].getItemMeta().getLore() == null){
-                itemsInFurnace[0].setAmount(-1);
-                List<Entity> near = furnace.getWorld().getEntities();
-                PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.VILLAGER_ANGRY, true, furnace.getLocation().getBlockX(), furnace.getLocation().getBlockY(), furnace.getLocation().getBlockZ(), 1, 1, 1, 10, 50, null);
-
-                for (Entity players : near) {
-                    if (players instanceof Player) {
-                        if (players.getLocation().distance(furnace.getLocation()) <= 10) {
-
-                            ((Player) players).updateInventory(); //it glitches with non-vanilla items, thts why we update
-                            players.sendMessage(worldciv + ChatColor.GRAY + " The egg(s) fried!");
-
-                         ((CraftPlayer) players).getHandle().playerConnection.sendPacket(packet);
-
-                        }
-
-                    }
-                }
-
-                Bukkit.getServer().getWorld("world").playSound(furnace.getLocation(), Sound.ENTITY_BLAZE_SHOOT, 5L, 0);
-
-                e.setCancelled(true);
-
-                return;
-            }
-
-           else if (itemsInFurnace[0].getItemMeta().getLore().get(0) == "imisspetra") {
-
-                List<Entity> near = furnace.getWorld().getEntities();
-                PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.VILLAGER_HAPPY, true, furnace.getLocation().getBlockX(), furnace.getLocation().getBlockY(), furnace.getLocation().getBlockZ(), 1, 1, 1, 10, 50, null);
-
-                for (Entity players : near) {
-                    if (players instanceof Player) {
-                        if (players.getLocation().distance(furnace.getLocation()) <= 10) {
-
-                            ((Player) players).updateInventory(); //it glitches with non-vanilla items, thts why we update
-                            players.sendMessage(worldciv + ChatColor.GRAY + " The egg(s) transformed into meat!");
-
-                            ((CraftPlayer) players).getHandle().playerConnection.sendPacket(packet);
-
-                        }
-
-                    }
-                }
-
-                Bukkit.getServer().getWorld("world").playSound(furnace.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 5L, 0);
-            }
+        if (itemsInFurnace[0] == null) {
+            return;
         }
 
+        if (itemsInFurnace[1] == null) {
+            List<Entity> near = furnace.getWorld().getEntities();
+            for (Entity players : near) {
+                if (players instanceof Player) {
+                    if (players.getLocation().distance(furnace.getLocation()) <= 10) {
+                        players.sendMessage(worldciv + ChatColor.GRAY + " There is no fuel on the furnace. Add more!");
+                    }
+                }
+            }
+            e.setCancelled(true);
+            furnaceInv.getHolder().setBurnTime((short) 20);
+            return;
+        }
+
+        if (itemsInFurnace[0].getType() == source && itemsInFurnace[1].getType() == fuel) {
+
+
+                List<Entity> near = furnace.getWorld().getEntities();
+
+                for (Entity players : near) {
+                    if (players instanceof Player) {
+                        if (players.getLocation().distance(furnace.getLocation()) <= 10) {
+
+                            if (itemsInFurnace[0].getItemMeta().getLore() == null || itemsInFurnace[1].getItemMeta().getLore() == null) {
+
+                                ((Player) players).updateInventory(); //it glitches with non-vanilla items, thts why we update
+                                players.sendMessage(worldciv + ChatColor.GRAY + " The ingot(s) didn't smelt!!");
+                                PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.VILLAGER_ANGRY, true, furnace.getLocation().getBlockX(), furnace.getLocation().getBlockY(), furnace.getLocation().getBlockZ(), 1, 1, 1, 10, 50, null);
+                                ((CraftPlayer) players).getHandle().playerConnection.sendPacket(packet);
+                                ((Player) players).playSound(furnace.getLocation(), Sound.ENTITY_BLAZE_SHOOT, 5L, 0);
+                                e.setCancelled(true);
+
+                            } else if (itemsInFurnace[0].getItemMeta().getLore().get(0).contains("refined iron ingot") && itemsInFurnace[1].getItemMeta().getLore().get(0).contains("Coke is a fuel")) {
+                                if (itemsInFurnace[1].getDurability() != 1) {
+                                    ((Player) players).updateInventory(); //it glitches with non-vanilla items, thts why we update
+                                    players.sendMessage(worldciv + ChatColor.GRAY + " The refined iron ingot(s) smelted into steel!");
+                                    PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.VILLAGER_HAPPY, true, furnace.getLocation().getBlockX(), furnace.getLocation().getBlockY(), furnace.getLocation().getBlockZ(), 1, 1, 1, 10, 50, null);
+                                    ((CraftPlayer) players).getHandle().playerConnection.sendPacket(packet);
+                                    ((Player) players).playSound(furnace.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 5L, 0);
+
+                                    if (itemsInFurnace[0].getAmount() > 1) {
+                                        furnaceInv.getHolder().setCookTime((short) 400);
+                                    }
+
+                                }
+
+                            } else if (itemsInFurnace[0].getItemMeta().getLore().get(0).contains("refined iron ingot") && itemsInFurnace[1].getItemMeta().getLore().get(0).contains("Activated Carbon is a fuel")) {
+                                if (itemsInFurnace[1].getDurability() == 1) { //charcoal
+                                    ((Player) players).updateInventory(); //it glitches with non-vanilla items, thts why we update
+                                    players.sendMessage(worldciv + ChatColor.GRAY + " The refined iron ingot(s) smelted into steel!");
+                                    PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.VILLAGER_HAPPY, true, furnace.getLocation().getBlockX(), furnace.getLocation().getBlockY(), furnace.getLocation().getBlockZ(), 1, 1, 1, 10, 50, null);
+                                    ((CraftPlayer) players).getHandle().playerConnection.sendPacket(packet);
+                                    ((Player) players).playSound(furnace.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 5L, 0);
+
+                                    if (itemsInFurnace[0].getAmount() > 1) {
+                                        furnaceInv.getHolder().setCookTime((short) 400);
+                                    }
+
+                                }
+                            }
+
+                        }
+                }
+            }
+        }
     }
 
-
     @EventHandler
-    public void onBurn(FurnaceBurnEvent e) {
-
+    public void onBurnEvent(FurnaceBurnEvent e){
         Furnace furnace = (Furnace) e.getBlock().getState();
         FurnaceInventory furnaceInv = (FurnaceInventory) furnace.getInventory();
 
         ItemStack[] itemsInFurnace = furnaceInv.getContents();
 
-        Material source = Material.EGG;
+        Material source = Material.IRON_INGOT;
         Material fuel = Material.COAL;
 
+        if (itemsInFurnace[0] == null || itemsInFurnace[1] == null) {
+            return;
+        }
 
-        if (itemsInFurnace[0].getType() == source && (e.getFuel().getType() == fuel)) {
+        if(itemsInFurnace[0].getItemMeta().getLore() == null | itemsInFurnace[1].getItemMeta().getLore() == null){
+            return;
+        }
 
-            if (itemsInFurnace[0].getItemMeta().getLore() == null) {
-                e.setCancelled(true);
-                e.setBurnTime(20);
-                return;
+        if(itemsInFurnace[1].getType() == Material.COAL && itemsInFurnace[1].getDurability() != 1 && itemsInFurnace[1].getItemMeta().getLore().get(0).contains("Coke is a fuel")){
+            for(Entity p : furnaceInv.getViewers()){
+                p.sendMessage(worldciv + ChatColor.GRAY + " You added coke (fuel) to the furnace!");
             }
-
-            for (HumanEntity players : furnaceInv.getViewers()) {
-
-                ((Player) players).updateInventory(); //it glitches with non-vanilla items, thts why we update
+            e.setBurnTime(200); //10s
+        }  else if(itemsInFurnace[1].getType() == Material.COAL && itemsInFurnace[1].getDurability() == 1 && itemsInFurnace[1].getItemMeta().getLore().get(0).contains("Activated Carbon")){
+            for(Entity p : furnaceInv.getViewers()){
+                p.sendMessage(worldciv + ChatColor.GRAY + " You added activated carbon to the furnace!");
             }
+            e.setBurnTime(100); //5s
+        }
 
-            if (itemsInFurnace[0].getItemMeta().getLore().get(0) == "imisspetra") {
+        if(itemsInFurnace[0].getType() == Material.IRON_INGOT && itemsInFurnace[0].getItemMeta().getLore().get(0).contains("refined iron ingot")){
+            furnaceInv.getHolder().setCookTime((short) 400); //20s
+        }
 
-                e.setBurnTime(200); //200 is one item
+
+    }
+
+    @EventHandler
+    public void onFurnaceClickEvent(InventoryClickEvent e) {
+        HumanEntity p = e.getWhoClicked();
+        if (e.getView().getType() == InventoryType.FURNACE) {
+            FurnaceInventory furnaceInv = (FurnaceInventory) e.getInventory();
+            ItemStack[] itemsInFurnace = furnaceInv.getContents();
+            int slot = e.getRawSlot();
+
+            if (furnaceInv.getHolder().getBurnTime() > 0) {
+                if(slot == 0  || slot == 1) {
+
+                    if (itemsInFurnace[0] == null || itemsInFurnace[1] == null){
+                        return;
+                    }
+
+                    if(itemsInFurnace[0].getType() == Material.IRON_INGOT && itemsInFurnace[1].getType() == Material.COAL)
+                    p.sendMessage(worldciv + ChatColor.GRAY + " It is too dangerous to pick this item while smelting!");
+                    e.setCancelled(true);
+                }
+
             }
         }
     }
-
-  /*  @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
-        HumanEntity p = e.getWhoClicked();
-        if (p instanceof Player) { //clicker is player
-
-            if (e.getView().getType() == InventoryType.FURNACE) { // detects if its a furnace
-
-                FurnaceInventory furnaceInv = (FurnaceInventory) e.getInventory(); //get anviilinv
-                ItemStack[] itemsInFurnace = furnaceInv.getContents();
-
-
-
-                int slot = e.getRawSlot(); //slots of anvil
-
-                Material source = Material.EGG;
-                Material fuel = Material.COAL;
-
-
-                if (slot == 0 || slot == 1 || e.isShiftClick() || e.isLeftClick() || e.isRightClick()) {
-
-                    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-                        @Override
-                        public void run() {
-
-                            try {
-
-                                if (itemsInFurnace[0] == null || itemsInFurnace[0].getItemMeta().getLore() == null || itemsInFurnace[0].getType() == null) {
-                                    if (p.getItemOnCursor().getType() == Material.EGG && furnaceInv.getHolder().getBurnTime() >= ((short) 1)) {
-                                        if (p.getItemOnCursor().getItemMeta().getLore().get(0) != "imisspetra") {
-                                            p.sendMessage(worldciv + ChatColor.GRAY + " The fuel ran out! I wonder why?");
-                                            furnaceInv.getHolder().setBurnTime((short) 20);
-                                            return;
-                                        }
-                                    }
-                                }
-
-                                if (p.getItemOnCursor().getType() == Material.EGG && furnaceInv.getHolder().getBurnTime() > 0) {
-                                    if (p.getItemOnCursor().getItemMeta().getLore() == null) {
-                                        p.sendMessage(worldciv + ChatColor.GRAY + " The fuel ran out! I wonder why?");
-                                        furnaceInv.getHolder().setBurnTime((short) 20);
-                                        return;
-                                    }
-                                }
-
-                                if (itemsInFurnace[1] == null) { // itemsInFurnace[1].getItemMeta().getLore() == null
-
-                                    if (itemsInFurnace[0] == null) {
-                                        return;
-                                    }
-                                    if (itemsInFurnace[0].getType() == Material.EGG) {
-                                        furnaceInv.getHolder().setBurnTime((short) 20);
-                                        return;
-                                    }
-                                }
-
-
-                                if (itemsInFurnace[0].getType() == source && (itemsInFurnace[1].getType() == fuel)) {
-                                    if (itemsInFurnace[0].getItemMeta().getLore() == null) {
-                                        furnaceInv.getHolder().setBurnTime((short) 20);
-                                        return;
-                                    }
-                                } else if (itemsInFurnace[0].getType() == source) {
-                                    furnaceInv.getHolder().setBurnTime((short) 20);
-                                    return;
-
-                                }
-
-                            } catch (Exception e) {
-
-                            }
-
-                        }
-                    }, 3);
-
-
-                }
-            }
-        }
-    }*/
-
 }
+
